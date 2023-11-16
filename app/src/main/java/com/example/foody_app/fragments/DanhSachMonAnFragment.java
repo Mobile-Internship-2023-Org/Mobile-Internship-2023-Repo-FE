5e7dadaa95adb6806ede0c62cae2eb9e6f22bd8b
DoannNguyen <nguyendoan27022001@gmail.com>
2023-11-16 09:27:58 +0700

    doan commit 16-11

--- app/src/main/java/com/example/foody_app/fragments/DanhSachMonAnFragment.java
+++ app/src/main/java/com/example/foody_app/fragments/DanhSachMonAnFragment.java
@@ -1,7 +1,11 @@
 package com.example.foody_app.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,52 +13,25 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.foody_app.R;
+import com.example.foody_app.activities.ThemMonAnActivity;
+import com.example.foody_app.activities.TimKiemActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DanhSachMonAnFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DanhSachMonAnFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private CardView cardViewSearch;
+    private FloatingActionButton fabAddFood;
 
     public DanhSachMonAnFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DanhSachMonAnFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DanhSachMonAnFragment newInstance(String param1, String param2) {
-        DanhSachMonAnFragment fragment = new DanhSachMonAnFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -62,5 +39,32 @@ public class DanhSachMonAnFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_danh_sach_mon_an, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        onBindView(view);
+
+        fabAddFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), ThemMonAnActivity.class);
+                startActivity(intent);
+            }
+        });
+        cardViewSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), TimKiemActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void onBindView(View view){
+        fabAddFood = view.findViewById(R.id.fabAddFood);
+        cardViewSearch = view.findViewById(R.id.cardViewSearch);
     }
 }
