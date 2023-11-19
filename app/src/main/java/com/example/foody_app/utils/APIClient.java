5b4b0b9907f17d91013bbcd06a375d8e0f6025f1
DoannNguyen <nguyendoan27022001@gmail.com>
2023-11-19 22:42:30 +0700

    import thư viện và tạo phương thức kết nối server

---
+++ app/src/main/java/com/example/foody_app/utils/APIClient.java
@@ -0,0 +1,22 @@
+package com.example.foody_app.utils;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APIClient {
+    private static Retrofit sRetrofit = null;
+    public static Retrofit getInstance(){
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        sRetrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.240.97:3000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(okHttpClient)
+                .build();
+        return sRetrofit;
+    }
+}
