c366fdfa01781386f5757b1d453dfd0830173087
Phapdn <106089682+PhapDang3@users.noreply.github.com>
2023-11-22 11:20:25 +0700

    (phapdv) chức năng update từ bàn phím người dùng

--- app/src/main/java/com/example/foody_app/utils/APIInterface.java
+++ app/src/main/java/com/example/foody_app/utils/APIInterface.java
@@ -1,11 +1,14 @@
 package com.example.foody_app.utils;
 
 import com.example.foody_app.models.FoodModel;
+import com.example.foody_app.models.RestaurantModel;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface APIInterface {
@@ -15,4 +18,10 @@ public interface APIInterface {
     //lấy dữ liệu món ăn theo thể loại
     @GET("/monanId/{foodId}")
     Call<FoodModel> getFoodById(@Path("foodId") String id);
+    // lấy thông tin nhà hàng
+    @GET("/nhahang")
+    Call<List<RestaurantModel>> getNhahang();
+    // update nhà hàng
+    @PUT("/updateNhahang")
+    Call<Void> updateNhahang(@Body RestaurantModel restaurantModel);
 }
